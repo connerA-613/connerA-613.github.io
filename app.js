b595f30dd81ce066695cbcf7107695fb615c5719
connerA-613 <74221595+connerA-613@users.noreply.github.com>
2023-09-07 13:38:04 -0400

    animations
    added fade-in animations

---
+++ app.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const animate = document.querySelectorAll('.animate');
+animate.forEach((el) => observer.observe(el));
